abb93a73e0a14511979a1d250b9fe9fc302629d5
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-02-20 00:31:09 +0300

    first touch

--- PatikaStore/App.tsx
+++ PatikaStore/App.tsx
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,104 +8,44 @@ import {
   Text,
   useColorScheme,
   View,
+  TextInput,
+  FlatList,
+  Image,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import style1 from './src/style1.style';
+import products from './src/productsJson';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+
+function App() {
+  
+  const [searchKey, setSearchKey] = useState("");
+
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+  <>
+      <Text style={style1.title} >Patika Store</Text>
+      <TextInput style={style1.search} onChangeText={(e) => setSearchKey(this.value)} value={searchKey}     
+      placeholder="Ara..." />
+
+      <>
+        <FlatList data={products} renderItem={({item, index}) => (
+          <View>
+             <Image style={style1.image} source={{uri:item.imgURL}} />
+            <Text>{item.title}</Text>
+            <Text>{item.price}</Text>
+          </View>
+        )} 
+        />
+         
+        
+      </>
+  </>
+    
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ PatikaStore/src/style1.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    title: {
+        color: "purple",
+        fontWeight: "bold",
+        fontSize: 34,
+        marginLeft: 10,
+
+    },
+    search: {
+        color: "white",
+    },
+    image: {
+        width: 200,
+        height: 100,
+    },
+});
