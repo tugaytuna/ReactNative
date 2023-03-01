0a8b34096cfaecad2706511136126cd712f688c8
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-03-01 21:07:50 +0300

    construction is done

---
+++ tarifka/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(){
+  
+
+  return (
+        <View>
+          <Text>Tarifka</Text>
+        </View>
+  );
+}
+
+
+
+export default App;
