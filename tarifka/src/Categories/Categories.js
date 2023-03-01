1c861b133ac97ab82466b6eca378cc13916a7682
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-03-01 21:48:00 +0300

    move loc

---
+++ tarifka/src/Categories/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native/types'
+
+import style from "./Categories.style"
+
+function Categories() {
+  return (
+    <Text style={style.contain}>Categories</Text>
+  )
+}
+
+export default Categories
---
+++ tarifka/src/Categories/Categories.style.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { StyleSheet } from "react-native"
+
+
+export default StyleSheet.create({
+    contain: {
+        backgroundColor: "gray"
+    }
+})
