f8c77752b31a91e12d0173c06fe6fe79424bfae0
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-03-10 18:49:28 +0300

    pages created

---
+++ kodwork/src/Pages/JobDetails/JobDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function JobDetails() {
+  return (
+    <View>
+        <Text>Job Details</Text>
+    </View>
+  )
+}
+
+export default JobDetails
---
+++ kodwork/src/Pages/Jobs/Jobs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Jobs() {
+  return (
+    <View>
+        <Text>Jobs</Text>
+    </View>
+  )
+}
+
+export default Jobs
