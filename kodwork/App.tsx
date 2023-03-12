e4f15fb0a556ab6c3a464940b222ea8a122eb6ab
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-03-12 16:03:07 +0300

    navigation added

--- kodwork/App.tsx
+++ kodwork/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import {Text,View} from 'react-native';
+import 'react-native-gesture-handler';
 
+import React from 'react';
+import {Text,View, Button} from 'react-native';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Jobs from './src/Pages/Jobs';
+import Favorites from './src/Pages/Favorites';
+
+const Drawer = createDrawerNavigator();
 
 function App(){
  
   return (
-    <View>
-      <Text>null</Text>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Jobs} />
+        <Drawer.Screen name="Favorites" component={Favorites} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  
   );
 }
 
