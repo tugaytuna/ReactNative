e04068e4171958db0f001c71065e120b0e8d1ec1
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-03-13 06:06:03 +0300

    custom hook created-fetch data

---
+++ kodwork/src/Components/getData/useFetch.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function useFetch(api) {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [err, setErr] = useState(null)
+
+    async function useFetchF(){
+      
+        axios.get(api)
+        .then((res) => {
+            let {data: data} = res;
+            setData(data);
+    
+        })
+        .catch((err) => {setErr(err)})
+    }
+
+    useEffect(() => {
+        useFetchF();
+    },[])
+
+
+    return {data, loading, err}
+}
+
+
+
+export default useFetch;
--- kodwork/src/Pages/Jobs/Jobs.js
+++ kodwork/src/Pages/Jobs/Jobs.js
@@ -2,11 +2,22 @@ import React from 'react'
 import { Button, Text, View } from 'react-native'
 import JobSingleItem from '../../Components/JobSingleItem'
 
+import useFetch from '../../Components/getData/useFetch'
+
 function Jobs({navigation}) {
+
+  const {data, loading, err} = useFetch("https://www.themuse.com/api/public/jobs?page=1");
+
+  const getDataHandle = () => {
+    console.log(data.results[1].name);
+  }
+
+
+
   return (
     <View>
         <Text>Jobs</Text>
-        <Button title='go To Job Details' onPress={() => {navigation.navigate("JobDetails")}} />
+        <Button title='go To Job Details' onPress={() => {getDataHandle()}} />
 
         <JobSingleItem/>
         <JobSingleItem/>
